9e6927a65998a5fdb1972bffe3bebfd138a2ffc5
Zain Saqib <xain.saqib@gmail.com>
2025-06-19 11:35:27 +0500

    NavBar and routing functionality added to App

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { CartPage } from './pages/cart-page/cart-page';
+import { Checkout } from './pages/checkout/checkout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'cart', component: CartPage },
+  { path: 'checkout', component: Checkout },
+  { path: '**', redirectTo: '' }
+];
